e26653ec3cf593af317ee992f42ca266396ebf3e
Joel <58995538+jtpotato@users.noreply.github.com>
2023-03-27 20:36:34 +1100

    do some cool stying things

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,8 +1,18 @@
 export default function Create() {
-    return (
-      <>
-  
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen w-screen">
+        <div className="flex flex-row space-x-4 w-3/4 justify-center">
+          <div className="flex flex-col h-48 w-1/4 space-y-2">
+            <textarea
+              placeholder="Insert prompt here..."
+              className="resize-none rounded-2xl h-full p-2"
+            ></textarea>
+            <button className=" bg-white rounded-2xl p-2">Generate 🪄</button>
+          </div>
+          <div className="h-96 w-96 bg-neutral-900 rounded-2xl"></div>
+        </div>
+      </div>
+    </>
+  );
+}
