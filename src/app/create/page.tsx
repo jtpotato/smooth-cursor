804c5ed309f5586741770aabcbe39b8a6d58bf54
Joel <58995538+jtpotato@users.noreply.github.com>
2023-04-05 14:05:20 +1000

    migrate to app folder

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,4 +1,6 @@
-import { useRouter } from "next/router";
+'use client'
+
+import { useRouter } from "next/navigation";
 import { useRef, useState } from "react";
 
 export default function Create() {
@@ -12,7 +14,7 @@ export default function Create() {
           inputs: prompt,
           options: {
             use_cache: false,
-            wait_for_model: false
+            wait_for_model: true
           }
         }),
       }
