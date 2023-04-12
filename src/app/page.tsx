4c92fb11180b6d825fc9ecf52888d368b5788363
Joel <58995538+jtpotato@users.noreply.github.com>
2023-04-12 17:12:35 +1000

    add new terminal features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@
 
 import { useEffect, useRef, useState } from "react";
 import "./caret.css";
-import terminalBehaviours from "./terminalBehaviours";
+import terminalBehaviours from "./terminal/terminalBehaviours";
 
 function Home() {
-  const [commandHistory, setCommandHistory] = useState<string[]>([""]);
-  const [commandHistoryIndex, setCommandHistoryIndex] = useState<number>(0);
-  const [caretXOffset, setCaretXOffset] = useState<number | undefined>(undefined);
-  const [caretYOffset, setCaretYOffset] = useState<number | undefined>(undefined);
+  const [currentCommand, setCurrentCommand] = useState<string>("");
+  const [caretXOffset, setCaretXOffset] = useState<number | undefined>(
+    undefined
+  );
+  const [caretYOffset, setCaretYOffset] = useState<number | undefined>(
+    undefined
+  );
   const commandInputTextRef = useRef<HTMLDivElement>(null);
   const commandInputBoxRef = useRef<HTMLDivElement>(null);
   const caretTargetRef = useRef<HTMLSpanElement>(null);
@@ -16,63 +19,58 @@ function Home() {
 
   // calculate caret offsets
   useEffect(() => {
-    let offsetTop = caretTargetRef.current!.offsetTop
-    let offsetLeft = caretTargetRef.current!.offsetLeft
-
-    // other weird edge case
-    if (commandHistory[commandHistoryIndex].endsWith(" ")) {
-      offsetLeft = offsetLeft + 10
-    }
-
-    // weird edge case
-    if (offsetLeft == 8) {
-      offsetTop =  (commandInputBoxRef.current!.offsetHeight/2) - (caretRef.current!.offsetHeight/2)
-      console.log(commandInputBoxRef.current!.clientHeight, caretRef.current!.offsetHeight)
-      console.log(offsetTop)
-    }
+    let offsetTop = caretTargetRef.current!.offsetTop;
+    let offsetLeft = caretTargetRef.current!.offsetLeft;
 
     setCaretYOffset(offsetTop);
     setCaretXOffset(offsetLeft);
-  }, [commandHistory, commandHistoryIndex])
+  }, [currentCommand]);
 
-  function keyDownHandler(e: any) { // stupid react hacks
-    const event: KeyboardEvent = e
-    const key: string = event.key;
-    if (key.length == 1) {
-      if (commandHistoryIndex !== commandHistory.length - 1) {
-        setCommandHistory([...commandHistory, commandHistory[commandHistoryIndex] + key.toLowerCase()])
-        setCommandHistoryIndex(commandHistory.length)
-      } else {
-        setCommandHistory([...commandHistory.slice(0, -1), commandHistory[commandHistoryIndex] + key.toLowerCase()])
-      }
-    }
-    if (key === "Backspace") {
-      if (commandHistory[commandHistoryIndex].length !== 0) {
-        if (commandHistoryIndex !== commandHistory.length - 1) {
-          setCommandHistory([...commandHistory, commandHistory[commandHistoryIndex].slice(0, -1)])
-          setCommandHistoryIndex(commandHistory.length)
-        }
-        else {
-          setCommandHistory([...commandHistory.slice(0, -1), commandHistory[commandHistoryIndex].slice(0, -1)]);
-        }
-      }
-    }
-    const commands = { commandHistoryIndex, setCommandHistoryIndex, commandHistory, setCommandHistory }
-    terminalBehaviours(event, commands)
+  function keyDownHandler(e: any) {
+    // stupid react hacks
+    const event: KeyboardEvent = e;
+    const commands = {
+      currentCommand,
+      setCurrentCommand
+    };
+    terminalBehaviours(event, commands);
   }
 
   return (
     <>
       <body className="bg-black w-screen h-screen" onKeyDown={keyDownHandler}>
-        <div ref={commandInputBoxRef} className="bg-neutral-900 bottom-0 absolute w-screen flex items-center p-2">
+        <div
+          ref={commandInputBoxRef}
+          style={{
+            backgroundColor: "#1f2937",
+            bottom: 0,
+            position: "absolute",
+            width: "100vw",
+            display: "flex",
+            alignItems: "center",
+            padding: "0.5rem",
+          }}
+        >
           <p
-            className="text-white inline-block p-2 pr-0 max-w-full"
+            style={{
+              color: "white",
+              display: "inline-block",
+              padding: "0.5rem",
+              paddingRight: 0,
+              width: "100%",
+              overflowWrap: "break-word",
+              minHeight: "2.5em"
+            }}
             ref={commandInputTextRef}
           >
-            {commandHistory[commandHistoryIndex]}
+            {currentCommand}
             <span ref={caretTargetRef} className="" />
           </p>
-          <div ref={caretRef} className="caret" style={{ left: caretXOffset, top: caretYOffset }} />
+          <div
+            ref={caretRef}
+            className="caret"
+            style={{ left: caretXOffset, top: caretYOffset }}
+          />
         </div>
       </body>
     </>
--- src/app/terminalBehaviours.ts
+++
@@ -1,6 +0,0 @@
-import { Dispatch, SetStateAction } from "react";
-
-export default function terminalBehaviours(event: KeyboardEvent, commands: { commandHistoryIndex: number, setCommandHistoryIndex: Dispatch<SetStateAction<number>>, commandHistory: string[], setCommandHistory: Dispatch<SetStateAction<string[]>> }) {
-    if (event.key === "ArrowDown") {
-    }
-}
