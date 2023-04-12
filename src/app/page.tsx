6de73e04fe6a402c46aa5400541db35624002c4b
Joel <58995538+jtpotato@users.noreply.github.com>
2023-04-12 14:21:56 +1000

    finish caret things. time for gh actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,40 @@
-'use client'
+"use client";
 
 import { useEffect, useRef, useState } from "react";
 import "./caret.css";
 
 function Home() {
-  const [text, setText] = useState("So this is great.")
-  const [caretOffset, setCaretOffset] = useState(0);
+  const [text, setText] = useState("");
+  const [caretOffset, setCaretOffset] = useState<number | undefined>(undefined);
   const commandInputTextRef = useRef<HTMLDivElement>(null);
 
-  function determineCaretOffset() {
-    setCaretOffset(commandInputTextRef.current!.clientWidth)
-  }
+  const widthMultiplierConstant = 9.6;
 
-  useEffect(() => {
-    determineCaretOffset()
-  }, [])
-  
-  function keyDownHandler(event) {
-    // if (event.getKey())
+  function keyDownHandler(event: any) {
+    const key: string = event.key;
+    if (key.length == 1) {
+      setCaretOffset(9.6 + (text.length + 1) * widthMultiplierConstant);
+      setText(text + key.toLowerCase());
+    }
+    if (key === "Backspace") {
+      if (text.length !== 0) {
+        setCaretOffset(9.6 + (text.length - 1) * widthMultiplierConstant);
+        setText(text.slice(0, -1));
+      }
+    }
   }
 
   return (
     <>
-      <body className="bg-black w-screen h-screen">
-        <div className="commandInputBox bg-neutral-900 bottom-0 absolute w-screen flex items-center p-2" onKeyDown={keyDownHandler}>
-          <p className="text-white w-fit inline-block" ref={commandInputTextRef}>
-          {text}
+      <body className="bg-black w-screen h-screen" onKeyDown={keyDownHandler}>
+        <div className="commandInputBox bg-neutral-900 bottom-0 absolute w-screen flex items-center h-12">
+          <p
+            className="text-white w-fit inline-block p-2 pr-0"
+            ref={commandInputTextRef}
+          >
+            {text}
           </p>
-          <div className="caret" style={{ left: caretOffset }}/>
+          <div className="caret" style={{ left: caretOffset }} />
         </div>
       </body>
     </>
